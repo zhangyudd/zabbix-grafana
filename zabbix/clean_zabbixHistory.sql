bb005fa24e7e4f48927197f4924fbe51aae85023
zhangyu <linuxzhangyu@163.com>
2020-05-13 09:33:55 +0800

    add history clean

---
+++ zabbix/clean_zabbixHistory.sql
@@ -0,0 +1,7 @@
+truncate table history;
+truncate table history_uint;
+truncate table alerts;
+truncate table trends_uint;
+truncate table trends;
+truncate table history_text;
+truncate table events;
